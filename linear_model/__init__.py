873d4bfa8dcb7d0b53511d6150e913c4c12c1f1c
Leandro Hermida <hermidal@cs.umd.edu>
2020-04-02 18:44:59 -0400

    Add ExtendedCoxnetSurvivalAnalysis entry to __init__

--- linear_model/__init__.py
+++ linear_model/__init__.py
@@ -1,5 +1,6 @@
 from ._cached import CachedCoxPHSurvivalAnalysis
-from ._coxnet import FastCoxPHSurvivalAnalysis
+from ._coxnet import ExtendedCoxnetSurvivalAnalysis, FastCoxPHSurvivalAnalysis
 
 __all__ = ['CachedCoxPHSurvivalAnalysis',
+           'ExtendedCoxnetSurvivalAnalysis',
            'FastCoxPHSurvivalAnalysis']
