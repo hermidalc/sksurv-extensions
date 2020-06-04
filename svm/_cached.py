54f002b070b98a283d2a3bef24ad9a1061c591b3
Leandro Hermida <hermidal@cs.umd.edu>
2020-06-04 14:48:38 -0400

    Updates to cached classes to use sklearn-extensions

--- svm/_cached.py
+++ svm/_cached.py
@@ -1,5 +1,5 @@
 from sksurv.svm import FastSurvivalSVM
-from .._cached import CachedFitMixin
+from sklearn_extensions.cached import CachedFitMixin
 
 
 class CachedFastSurvivalSVM(CachedFitMixin, FastSurvivalSVM):
@@ -7,8 +7,8 @@ class CachedFastSurvivalSVM(CachedFitMixin, FastSurvivalSVM):
     def __init__(self, memory, alpha=1, rank_ratio=1.0, fit_intercept=False,
                  max_iter=20, verbose=False, tol=None, optimizer=None,
                  random_state=None, timeit=False):
-        self.memory = memory
         super().__init__(alpha=alpha, rank_ratio=rank_ratio,
                          fit_intercept=fit_intercept, max_iter=max_iter,
                          verbose=verbose, tol=tol, optimizer=optimizer,
                          random_state=random_state, timeit=timeit)
+        self.memory = memory
